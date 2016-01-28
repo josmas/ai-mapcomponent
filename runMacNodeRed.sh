d43ba31c7306d7fc028d1caa2672b9f093225e29
josmas <josmasflores@gmail.com>
2016-01-28 21:19:01 +0000

    container for node environment (to run build tools)

---
+++ runMacNodeRed.sh
@@ -0,0 +1,16 @@
+# Colours for Docker tabs. Adapted from https://gist.github.com/pablete/5871811
+function tabc() {
+  NAME=$1; if [ -z "$NAME" ]; then NAME="Default"; fi
+  echo -e "\033]50;SetProfile=$NAME\a"
+}
+
+function colordocker() {
+  tabc docker
+  docker $*
+  tabc
+}
+
+# Actual call to docker run
+colordocker run -it --rm \
+  -v /Users/jos/development/AppInventor/ai-mapcomponent/:/home/jos/code \
+  node:5.1.0 /bin/bash
